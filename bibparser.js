6b6873df85fdcbbb751e8ec20ce05eabf6d5c637
Germán Leiva <leivagerman@gmail.com>
2025-04-11 15:11:50 +1000

    Changed style and other content

--- bibparser.js
+++ bibparser.js
@@ -5,25 +5,28 @@ const bibtexData = `
   booktitle={In Proceedings of the 2024 CHI Conference on Human Factors in Computing Systems (CHI '24)},
   year={2024},
   addendum = {Article 798, 1–16},
-  keywords = {full-paper}
+  keywords = {full-paper},
+  doi = {10.1145/3613904.3642293}
 }
 
 @inproceedings{wong2024,
-  title={Distributed MR Collaboration at Scale: Practice-informed Patterns for Blended F-formations},
+  title={Practice-informed Patterns for Organising Large Groups in Distributed Mixed Reality Collaboration},
   author={Emily, Wong and Esquivel, Juan Sánchez and Leiva, Germán and Grønbæk, Jens Emil Sloth and Velloso, Eduardo},
   booktitle={In Proceedings of the 2024 CHI Conference on Human Factors in Computing Systems (CHI '24)},
   year={2024},
-addendum= {Article 1030, 1–18},
-  keywords = {full-paper}
+  addendum= {Article 1030, 1–18},
+  keywords = {full-paper},
+  doi = {10.1145/3613904.3642502}
 }
 
 @inproceedings{mackay2024,
-  title={Rapid Prototyping with VideoClipper: In-camera Storyboarding and Video Capture},
+  title={VideoClipper: Rapid Prototyping with the ''Editing-in-the-Camera'' Method},
   author={Mackay, Wendy E. and Battut, Alexandre and Leiva, Germán and Beaudouin-Lafon, Michel},
   booktitle={In Proceedings of the 2024 CHI Conference on Human Factors in Computing Systems (CHI '24)},
   year={2024},
-addendum = {Article 228, 1–14},
-  keywords = {full-paper}
+  addendum = {Article 228, 1–14},
+  keywords = {full-paper},
+  doi = {10.1145/3613904.3642458}
 }
 
 @inproceedings{griggio2011programming,
@@ -32,7 +35,8 @@ addendum = {Article 228, 1–14},
   booktitle={Proceedings of the International Workshop on Smalltalk Technologies},
   pages={1--5},
   year={2011},
-  keywords = {peer-paper}
+  keywords = {peer-paper},
+  doi = {10.1145/2166929.2166934}
 }
 
 @inproceedings{griggio2015towards,
@@ -42,7 +46,8 @@ addendum = {Article 228, 1–14},
   pages={1513--1518},
   year={2015},
   organization={ACM},
-  keywords = {peer-paper}
+  keywords = {peer-paper},
+  doi = {10.1145/2702613.2732925}
 }
 
 @inproceedings{griggio2016uist,
@@ -51,7 +56,8 @@ addendum = {Article 228, 1–14},
   booktitle={UIST 2016-29th ACM Symposium on User Interface Software and Technology},
   pages={59--60},
   year={2016},
-  keywords = {peer-paper}
+  keywords = {peer-paper},
+  doi = {10.1145/2984751.2985703}
 }
 
 @inproceedings{maudet2017design,
@@ -60,8 +66,9 @@ addendum = {Article 228, 1–14},
   booktitle={Proceedings of the 2017 ACM Conference on Computer Supported Cooperative Work and Social Computing},
   year={2017},
   organization={ACM},
-pages = {630–641},
-  keywords = {full-paper}
+  pages = {630–641},
+  keywords = {full-paper},
+  doi = {10.1145/2998181.2998190}
 }
 
 @inproceedings{leiva2018montage,
@@ -69,17 +76,19 @@ pages = {630–641},
   author={Leiva, Germán and Beaudouin-Lafon, Michel},
   booktitle={Proceedings of the 31st Annual ACM Symposium on User Interface Software and Technology},
   year={2018},
-pages = {675–682},
-  keywords = {full-paper}
+  pages = {675–682},
+  keywords = {full-paper},
+  doi = {10.1145/3242587.3242613}
 }
 
 @phdthesis{leiva2018interactive,
   title={Interactive Prototyping of Interactions: from Throwaway Prototypes to Takeaway Prototyping},
-  author={Leiva, Germán},
+  author={Leiva, Germán},
   year={2018},
   school={Universit{\'e} Paris-Saclay (ComUE)},
-  NUMBER = {2018SACLS551},
-  keywords = {full-paper}
+  number = {2018SACLS551},
+  keywords = {full-paper},
+  url = {https://theses.hal.science/tel-02122823v1/file/74791_LEIVA_2018_archivage.pdf}
 }
 
 @article{leiva2019enact,
@@ -91,7 +100,8 @@ pages = {675–682},
   pages={19},
   year={2019},
   publisher={ACM},
-  keywords = {full-paper}
+  keywords = {full-paper},
+  doi = {10.1145/3310276}
 }
 
 @inproceedings{leiva2020pronto,
@@ -99,8 +109,9 @@ pages = {675–682},
   author={Leiva, Germán and Nguyen, Cuong and Habib Kazi, Rubaiat and Asente, Paul},
   booktitle={CHI '20: Proceedings of the 2020 CHI Conference on Human Factors in Computing Systems},
   year={2020},
-pages = {1–13},
-  keywords = {full-paper}
+  pages = {1–13},
+  keywords = {full-paper},
+  doi = {10.1145/3313831.3376160}
 }
 
 @misc{nguyen2022augmented,
@@ -109,23 +120,27 @@ pages = {1–13},
   year={2022},
   month=feb # "~1",
   note={US Patent 11,238,657},
-    keywords={other-patents}
+  keywords={other-patents},
+  url={https://patents.google.com/patent/US20210272363A1/en}
 }
+
 
 @inproceedings{leiva2021rapido,
   title={Rapido: Prototyping Interactive AR Experiences through Programming by Demonstration},
   author={Leiva, Germán and Grønbæk, Jens Emil and Klokmose, Clemens Nylandsted and Nguyen, Cuong and Kazi, Rubaiat Habib and Asente, Paul},
   booktitle={The 34th Annual ACM Symposium on User Interface Software and Technology},
   year={2021},
-pages = {626–637},
-  keywords = {full-paper}
+  pages = {626–637},
+  keywords = {full-paper},
+  doi = {10.1145/3472749.3474774}
 }
 
 @misc{leiva2015gaze,
   title={Gaze-supported pointing devices for day-to-day computer interaction},
   author={Leiva, Germán},
   year={2015},
-  keywords = {}
+  keywords = {},
+  url = {https://www.diva-portal.org/smash/get/diva2:859536/FULLTEXT01.pdf}
 }
 
 @inproceedings{leiva2018towards,
@@ -133,7 +148,8 @@ pages = {626–637},
   author={Leiva, Germán and Maudet, Nolwenn and Beaudouin-Lafon, Michel},
   booktitle={Workshop: Digital Tools in Collaborative Creative Work},
   year={2018},
-  keywords = {peer-paper}
+  keywords = {peer-paper},
+  url = {https://hal.science/hal-03688610v1/file/Leiva%2Cetal-PositionPaper.pdf}
 }
 
 @inproceedings{gronbaek2023partially,
@@ -141,10 +157,11 @@ pages = {626–637},
   author={Grønbæk, Jens Emil Sloth and Pfeuffer, Ken and Velloso, Eduardo and Astrup, Morten and Pedersen, Melanie Isabel Sønderkær and Kjær, Martin and Leiva, Germán and Gellersen, Hans},
   booktitle={In Proceedings of the 2023 CHI Conference on Human Factors in Computing Systems (CHI '23)},
   volume={456},
-numpages = {16},
-addendum = {p. 16},
+  numpages = {16},
+  addendum = {p. 16},
   year={2023},
-  keywords = {full-paper}
+  keywords = {full-paper},
+  doi = {10.1145/3544548.3581515}
 }
 
 @inproceedings{lunding2022projectar,
@@ -152,29 +169,31 @@ addendum = {p. 16},
   author={Lunding, Mille Skovhus and Leiva, Germán and Grønbæk, Jens Emil Sloth and Petersen, Marianne Graves},
   booktitle={Adjunct Proceedings of the 2022 Nordic Human-Computer Interaction Conference},
   year={2022},
-articleno = {55}, 
-numpages = {5}, 
-addendum= {Artice 55, 5},
-  keywords = {peer-paper}
+  articleno = {55}, 
+  numpages = {5}, 
+  addendum= {Article 55, 5},
+  keywords = {peer-paper},
+  doi = {10.1145/3547522.3547679}
 }
 
 @inproceedings{vrloop,
-title = "VRLOOP: Immersive Virtual Reality for Learning Object-Oriented Programming",
-abstract = "Novice students often report difficulties applying abstract concepts of Object-Oriented Programming (OOP). Several studies highlight the potential of Immersive Virtual Reality (VR) as a valuable tool for supporting classroom teaching through novel and engaging learning environments. However, combining interactive learning in VR with concepts from OOP is challenging. We present VRLOOP (VR Learning of OOP), an immersive system to learn OOP concepts in a collaborative and embodied environment. The design of VRLOOP combines principles from HCI and educational psychology. VRLOOP supports enactive rather than symbolic learning, i.e. the use of textual code. We conducted an initial pilot study with 14 students to evaluate VRLOOP's usability and gather subjective feedback. 93\% reported that VRLOOP made the content from the class easier to understand. Finally, we discuss limitations and directions for future research on VR environments for learning programming concepts.",
-keywords = "Virtual Reality, Immersive learning, Collaborative learning, Computer science education, Object-oriented programming",
-author = "Lui, Lasse F. and Vittoria Frau and Guido Makransky and Konstantinos Koumaditis and Germán Leiva",
-year = "2025",
-month = jan,
-day = "1",
-language = "English",
-booktitle = "Proceedings - 2025 IEEE Conference on Virtual Reality and 3D User Interfaces Abstracts and Workshops, VRW 2025",
-publisher = "IEEE",
-keywords = {peer-paper}
+  title = "VRLOOP: Immersive Virtual Reality for Learning Object-Oriented Programming",
+  abstract = "Novice students often report difficulties applying abstract concepts of Object-Oriented Programming (OOP). Several studies highlight the potential of Immersive Virtual Reality (VR) as a valuable tool for supporting classroom teaching through novel and engaging learning environments. However, combining interactive learning in VR with concepts from OOP is challenging. We present VRLOOP (VR Learning of OOP), an immersive system to learn OOP concepts in a collaborative and embodied environment. The design of VRLOOP combines principles from HCI and educational psychology. VRLOOP supports enactive rather than symbolic learning, i.e. the use of textual code. We conducted an initial pilot study with 14 students to evaluate VRLOOP's usability and gather subjective feedback. 93\% reported that VRLOOP made the content from the class easier to understand. Finally, we discuss limitations and directions for future research on VR environments for learning programming concepts.",
+  keywords = "Virtual Reality, Immersive learning, Collaborative learning, Computer science education, Object-oriented programming",
+  author = "Lui, Lasse F. and Vittoria Frau and Guido Makransky and Konstantinos Koumaditis and Germán Leiva",
+  year = "2025",
+  month = jan,
+  day = "1",
+  language = "English",
+  booktitle = "Proceedings - 2025 IEEE Conference on Virtual Reality and 3D User Interfaces Abstracts and Workshops, VRW 2025",
+  publisher = "IEEE",
+  keywords = {peer-paper},
+  url = {https://pure.au.dk/portal/da/publications/vrloop-immersive-virtual-reality-for-learning-object-oriented-pro}
 }
 `;
 
 function decodeLatexAccents(text) {
-return text
+  return text
     .replace(/\\'{a}/g, 'á')
     .replace(/\\'{e}/g, 'é')
     .replace(/\\'{i}/g, 'í')
@@ -184,47 +203,57 @@ return text
 }
 
 function reorderAuthors(authorString) {
-const authors = authorString.split(/\s+and\s+/i).map(name => {
+  const authors = authorString.split(/\s+and\s+/i).map(name => {
     const parts = name.split(',').map(p => p.trim());
     return parts.length === 2 ? `${parts[1]} ${parts[0]}` : name.trim();
-});
-return authors.length > 1
+  });
+  return authors.length > 1
     ? authors.slice(0, -1).join(', ') + ', and ' + authors.slice(-1)
     : authors[0];
 }
 
 function renderPublications(bibData) {
-const entries = bibData.split('@').slice(1).map(entry => {
+  const entries = bibData.split('@').slice(1).map(entry => {
     const titleMatch = entry.match(/title\s*=\s*[{"]([^"}]+)[}"]/i);
     const authorMatch = entry.match(/author\s*=\s*[{"]([^"}]+)[}"]/i);
     const yearMatch = entry.match(/year\s*=\s*[{"]?(\d{4})[}"]?/i);
     const doiMatch = entry.match(/doi\s*=\s*[{"]([^"}]+)[}"]/i);
+    const urlMatch = entry.match(/url\s*=\s*[{"]([^"}]+)[}"]/i);
 
     const title = titleMatch ? decodeLatexAccents(titleMatch[1]) : 'No title';
     const authors = reorderAuthors(decodeLatexAccents(authorMatch?.[1] || 'Unknown authors'));
     const year = yearMatch ? parseInt(yearMatch[1]) : 0;
+    const url = urlMatch ? urlMatch[1] : '';
     const doi = doiMatch ? doiMatch[1] : '';
-    const doiUrl = doi ? `https://doi.org/${doi}` : "#";
 
-    return { title, authors, year, doiUrl };
-});
+    let titleHTML = '';
+    if (url) {
+      titleHTML = `<a href="${url}" target="_blank">${title}</a>`;
+    } else if (doi) {
+      titleHTML = `<a href="https://doi.org/${doi}" target="_blank">${title}</a>`;
+    } else {
+      titleHTML = `${title}`;
+    }
 
-entries.sort((a, b) => b.year - a.year); // sort by year descending
+    return { titleHTML, authors, year };
+  });
 
-const publicationList = document.getElementById('publications');
-publicationList.innerHTML = '';
+  entries.sort((a, b) => b.year - a.year); // sort by year descending
 
-entries.forEach(({ title, authors, year, doiUrl }) => {
+  const publicationList = document.getElementById('publications');
+  publicationList.innerHTML = '';
+
+  entries.forEach(({ titleHTML, authors, year }) => {
     const div = document.createElement('div');
     div.className = 'pub-entry';
     div.innerHTML = `
-    <strong><a href="${doiUrl}" target="_blank">${title}</a></strong><br>
-    <em>${authors}</em> (${year})
+      <strong>${titleHTML}</strong><br>
+      <em>${authors}</em> (${year})
     `;
     publicationList.appendChild(div);
-});
+  });
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-renderPublications(bibtexData);
+  renderPublications(bibtexData);
 });
